d59d0841fb7f0553c5612cbcb6a32a6b0531f93f
John Laceda <john.laceda@gmail.com>
2019-04-05 03:20:53 -0700

    adding local external libs

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCmoEjZSolqq7cct3T1_E6k1YA5YoTqoV0",
+	authDomain: "trains-ddf4e.firebaseapp.com",
+	databaseURL: "https://trains-ddf4e.firebaseio.com",
+	projectId: "trains-ddf4e",
+	storageBucket: "trains-ddf4e.appspot.com",
+	messagingSenderId: "547577775317"
+};
+firebase.initializeApp(config);
