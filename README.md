d9927d93a8ac403d431d263ad455cc989af85690
John Laceda <john.laceda@gmail.com>
2019-04-05 23:02:40 -0700

    adding to  schedule table from db.

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ UW Coding Bootcamp Homework - Train Scheduler
 * Try adding `update` and `remove` buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
 * As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
 ## Plan
-- ~~basic layout with bootstrap~~
-- ~~make firebase rtdb~~
-- ~~add click handler to submit button to make a record in firebase~~
-- use firebase event child_added to add schedules on table
-- add calculations for when the next train will arrive
-- add an auto update times feature
-- delete a schedule
-- update a schedule
-- firebase auth
+- [X] basic layout with bootstrap
+- [X] make firebase rtdb
+- [X] add click handler to submit button to make a record in firebase
+- [X] use firebase event child_added to add schedules on table
+- [X] add calculations for when the next train will arrive
+- [ ] add an auto update times feature
+- [ ] able to delete a schedule from ui
+- [ ] able to update a schedule from ui
+- [ ] firebase auth
--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/// <reference path="jquery.js"/>
+/// <reference path="firebase-app.js"/>
+/// <reference path="firebase-database.js"/>
+/// <reference path="moment.js"/>
+
 // Initialize Firebase
 const config = {
 	apiKey: "AIzaSyCmoEjZSolqq7cct3T1_E6k1YA5YoTqoV0",
@@ -11,6 +16,14 @@ firebase.initializeApp(config);
 
 let databaseRef = firebase.database().ref();
 
+const scheduleTemplate = ({trainName, destination, frequency, nextArrival, minutesAway}) => `
+<td>${trainName}</td>
+<td>${destination}</td>
+<td>${frequency}</td>
+<td>${nextArrival}</td>
+<td>${minutesAway}</td>
+`;
+
 // on document ready
 $(()=>{
 
@@ -18,9 +31,39 @@ $(()=>{
 	databaseRef.on("child_added", (childSnapshot) =>
 	{
 		var schedule = childSnapshot.val();
-		console.log(schedule);
-		// TODO: do calculations
-		// TODO: make a new table entry
+		//do time calculations
+		let nextArrival = null;
+		let minutesAway = null;
+
+		// find the first train today time
+		let todayAtMidnight = moment().startOf('day');
+		let firstTrainTimeDuration = moment.duration(schedule.firstTrainTime);
+		let firstTrainToday = todayAtMidnight.add(firstTrainTimeDuration);
+
+		// if the first train hasn't departed yet, use that time for cals
+		if (moment() < firstTrainToday)
+		{
+			nextArrival = firstTrainToday.format("HH:mm");
+			minutesAway = firstTrainToday.diff(moment(), 'minutes');
+		}
+		// if the first train has departed today use the formula:
+		// frequencyInMinutes - ( (now - firstTrainToday) % frequencyInMinutes )
+		// thanks greg!
+		else
+		{
+			let diffInMins = moment().diff(firstTrainToday, 'minutes');
+			let frequencyInMinutes = parseInt(schedule.frequency)
+			minutesAway = frequencyInMinutes - (diffInMins % frequencyInMinutes);
+			nextArrival = moment().add(minutesAway, 'minutes').format("HH:mm");
+		}
+
+		// put data in the object
+		schedule.minutesAway = minutesAway;
+		schedule.nextArrival = nextArrival;
+
+		// make a new table entry
+		let scheduleTr = $("<tr>").html([schedule].map(scheduleTemplate).join(''));
+		$("#schedules").append(scheduleTr);
 	});
 
 	$("#submitButton").click((event) =>
@@ -43,7 +86,11 @@ $(()=>{
 			frequency: frequency
 		});
 
-		// TODO: clear inputs
+		// clear inputs
+		$("#trainName-input").val('');
+		$("#destination-input").val('');
+		$("#firstTrainTime-input").val('');
+		$("#frequency-input").val('');
 	});
 
 });
